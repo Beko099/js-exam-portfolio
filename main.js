9b8e8ffb84987c065d37122d73cd54bcc9d7daf0
beko099 <begali09092004@gmail.com>
2023-02-02 00:04:56 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const pics = [
+    'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1020&q=80',
+    'https://images.unsplash.com/photo-1647891941746-fe1d53ddc7a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    'https://images.unsplash.com/photo-1581044777550-4cfa60707c03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80',
+    'https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80',
+    'https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80',
+    'https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    'https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  ]
+
+  const allImage = []
+
+
+for (let i = 0; i < pics.length; i++) {
+  const img = document.createElement('div')
+  img.style.backgroundImage = `url(${pics[i]})`
+  document.querySelector('.pictures').append(img)
+  img.classList = 'img-portfolio'
+  allImage.push(img)
+  
+    
+}
+// console.log(allImage[])
+
+let imed = 0
+
+for (let i = 0; i<allImage.length; i++) {
+  const box = document.createElement('button')
+  box.classList = 'filter'
+  const lupa = document.createElement('div')
+  
+  lupa.classList = 'lupa'
+  allImage[i].append(box)
+  box.append(lupa)
+  console.log(box[i])
+  allImage[i].addEventListener('mouseenter', () => {
+    box.style.display = 'block'
+  })
+  allImage[i].addEventListener('mouseleave', () => {
+    box.style.display = 'none'
+  })
+  
+}
+
+
+
+
+
+ 
+
+
+
+
